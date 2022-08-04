4c6b99642b7e32a114a303a6d13d765c62a91c63
SwapnilZimal <swapnilz1999@gmail.com>
2022-08-04 09:58:07 +0530

    initial commit

---
+++ Zimal.java
@@ -0,0 +1,13 @@
+class Zimal {
+  public static void main (String... args){
+    String name = args[0];
+    int empid = Integer.parseInt(args[1]);
+    float salary = Float.parseFloat(args[2]);
+
+    System.out.println("Name: " +name);
+    System.out.println("EmpID: " +empid);
+    System.out.println("Salary: " +salary);
+    
+    
+  }
+}
